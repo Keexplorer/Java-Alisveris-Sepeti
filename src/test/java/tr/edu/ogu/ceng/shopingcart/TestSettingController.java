b1c7f63611306e67fe04477d1f7e2e33490a5f0f
Keexplorer <pekincekerim@gmail.com>
2024-12-05 13:45:41 +0300

    ip'ler eklendi

--- src/test/java/tr/edu/ogu/ceng/shopingcart/TestSettingController.java
+++ src/test/java/tr/edu/ogu/ceng/shopingcart/TestSettingController.java
@@ -1,5 +1,6 @@
 package tr.edu.ogu.ceng.shopingcart;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import tr.edu.ogu.ceng.shopingcart.controller.UserController;
-import tr.edu.ogu.ceng.shopingcart.dto.UserDto;
-import tr.edu.ogu.ceng.shopingcart.service.UserService;
+import tr.edu.ogu.ceng.shopingcart.controller.SettingController;
+import tr.edu.ogu.ceng.shopingcart.dto.SettingDto;
+import tr.edu.ogu.ceng.shopingcart.service.SettingService;
 
-import java.time.LocalDateTime;
-
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class TestUserController extends Container {
+public class TestSettingController {
 
 }
 
