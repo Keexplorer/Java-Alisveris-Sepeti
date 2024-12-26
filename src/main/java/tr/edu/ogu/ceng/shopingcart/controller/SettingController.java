dca0a6ffdbb3cad5d52545babd537031f2206fb0
Keexplorer <pekincekerim@gmail.com>
2024-12-26 16:15:06 +0300

    Setting Controller düzenlendi

--- src/main/java/tr/edu/ogu/ceng/shopingcart/controller/SettingController.java
+++ src/main/java/tr/edu/ogu/ceng/shopingcart/controller/SettingController.java
@@ -1,12 +1,11 @@
 package tr.edu.ogu.ceng.shopingcart.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import jdk.jfr.SettingControl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import tr.edu.ogu.ceng.shopingcart.dto.SettingDto;
 import tr.edu.ogu.ceng.shopingcart.entity.Setting;
 import tr.edu.ogu.ceng.shopingcart.service.SettingService;
@@ -18,10 +17,22 @@ public class SettingController {
     private final SettingService settingService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "Example Endpoint", description = "This is an example endpoint for demonstration purposes.")
     public SettingDto getSetting(@PathVariable Long id){
 
         return settingService.getSetting(id);
-
     }
+
+    @PostMapping
+    @Operation(summary = "Add a new user", description = "Saves a new user to the database.")
+    public ResponseEntity<Setting> addUser(@RequestBody SettingDto settingDto){
+        Setting setting = new Setting();
+        setting.setSettingKey(settingDto.getSettingKey());
+        setting.setSettingValue(settingDto.getSettingValue());
+
+        Setting savedSetting = settingService.saveSetting(setting);
+        return ResponseEntity.ok(savedSetting);
+    }
+
 
 }
