7f334d9f9c6211b939d05f1bca4307446bc57a33
Ozan <hozansismanoglu05@gmail.com>
2024-12-26 17:18:22 +0300

    Product

--- src/main/java/tr/edu/ogu/ceng/shopingcart/controller/ProductController.java
+++ src/main/java/tr/edu/ogu/ceng/shopingcart/controller/ProductController.java
@@ -1,12 +1,33 @@
 package tr.edu.ogu.ceng.shopingcart.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tr.edu.ogu.ceng.shopingcart.dto.ProductDto;
+import tr.edu.ogu.ceng.shopingcart.service.ProductService;
 
 @RestController
 @RequestMapping("/API/v1/Product")
 @RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Product çekme", description = "Product çekme")
+    public ProductDto getProduct(@PathVariable Long id){
+
+        return productService.getProduct(id);
+
+
+
+    }
+
+
+
+
+
 }
