31d51f1b6795a76a73d04af3a7f064ae783c5141
orhan <orhangun009@gmail.com>
2024-12-26 17:16:24 +0300

    category ve cartitem controller-service

--- src/main/java/tr/edu/ogu/ceng/shopingcart/service/CategoryService.java
+++ src/main/java/tr/edu/ogu/ceng/shopingcart/service/CategoryService.java
@@ -2,8 +2,11 @@ package tr.edu.ogu.ceng.shopingcart.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import tr.edu.ogu.ceng.shopingcart.dto.CategoryDto;
+import tr.edu.ogu.ceng.shopingcart.dto.SettingDto;
 import tr.edu.ogu.ceng.shopingcart.entity.Category;
 
+import tr.edu.ogu.ceng.shopingcart.entity.Setting;
 import tr.edu.ogu.ceng.shopingcart.repository.CategoryRepository;
 
 
@@ -14,9 +17,34 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
 
 
+    public CategoryDto getCategory(Long id){
+        Category category =categoryRepository.getReferenceById(id);
+
+        return convertToDto(category);
+
+    }
+
     public Category saveCategory(Category category){
 
         return categoryRepository.save(category);
+
+    }
+
+    private CategoryDto convertToDto(Category category) {
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setId(category.getId());
+        categoryDto.setName(category.getName());
+        categoryDto.setCreatedAt(category.getCreatedAt());
+        categoryDto.setCreatedBy(category.getCreatedBy());
+        categoryDto.setDeletedBy(category.getDeletedBy());
+        categoryDto.setUpdatedAt(category.getUpdatedAt());
+        categoryDto.setVersion(category.getVersion());
+        categoryDto.setUpdatedBy(category.getUpdatedBy());
+        categoryDto.setDeletedAt(category.getDeletedAt());
+        return categoryDto;
+        //settingDto.setSettingKey(setting.getSettingKey());
+        //settingDto.setSettingValue(setting.getSettingValue());
+        //return settingDto;
 
     }
 
